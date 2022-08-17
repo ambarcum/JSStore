817da6fbc274c6d4912368d9ea0407f361f6fae7
ambarcum <98892164+ambarcum@users.noreply.github.com>
2022-08-17 13:06:08 +0300

    1ый урок

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const products = [
+    { id: 1, title: 'Laptop', price: 30000 },
+    { id: 2, title: 'Mouse', price: 2000 },
+    { id: 3, title: 'Keyboard', price: 2500 },
+    { id: 4, title: 'Phone', price: 20000 }
+];
+
+const renderProduct = (product) =>
+    `<div class="products_item">
+    <h4 class="item_heading">${product.title}</h4>
+    <p class="item_price">${product.price}</p>
+    <button class="item_butt">Купить</button>
+ </div>`
+
+
+const renderPage = (list) => {
+    const productsList = list.map(product => renderProduct(product));
+    document.querySelector('.products').innerHTML = productsList.join('');
+}
+
+renderPage(products);
